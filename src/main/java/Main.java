8e4b9e98a7f28e07e5c66946398ef9b4704b0951
Christy <christy@Zipcoders-MacBook-Pro-6.local>
2023-10-27 18:10:22 -0400

    need more change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
-public class Main {
+import java.util.Scanner;
+import java.util.Random;
 
-    public static void main(String[] args){
-        
+
+public class Main {
+    public static void main(String[] args) {
     }
-}
+
+    int max = 25;
+    int randomNumber = (int)(Math.random().max);
+
+      Random rand   = new Random();
+    Scanner scanner = new Scanner ( System.in);
+    System.out.print(20);
+
+
+
+
+    }
